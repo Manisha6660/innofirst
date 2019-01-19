7f7937252d53474f36c7c3e500e0204719c8e293
Manisha6660 <46329362+Manisha6660@users.noreply.github.com>
2019-01-19 19:18:48 +0530

    C:\Users\user1\Desktop\html\office assgn\jquery

---
+++ page.js
@@ -0,0 +1,64 @@
+$(document).ready(function()
+		{ $("#submit").click(function()
+	      { 
+		      alert("Please fill all the fields!!!" +  $(".text").css("background-color","red"));
+	       
+	  });
+	});
+
+
+
+$(document).ready(function()
+	  {
+        $(".full").hide();
+          $(".more").click(function() 
+          {  $(".full").show();
+             $(".more").hide();
+      });
+       });
+       
+  $(document).ready(function()
+	  { $("#home").click(function()
+	      {   $(".subheader").css(" background-color","yellow");
+	       
+	  }); 
+		});
+      /*
+	  $("#services").click(function()
+	      {    $("#subheader").css(" background-color","green");
+	       
+	  });
+
+	  $(document).ready(function()
+	  {
+	  $("#profiles").click(function()
+	      {   $("#subheader").css(" background-color","white");
+	  });
+	});
+	  $(document).ready(function()
+	  {
+	  $("#contact").click(function()
+	      {   $("#subheader").css(" background-color","red");
+	       
+	  });
+		});
+     
+       $(document).ready(function(){
+      $("#arleft").click(function()
+       {  $("#banner").animate({"background-color", "red"}, "slow");
+   });
+   });
+     $(document).ready(function(){
+      $("#arright").click(function()
+       {  $("#banner").css("background-color", "red");
+   });
+});
+ 
+ 
+	$(document).ready(function()
+	  { $("#fhome").click(function()
+	      {   $(".subfooter").css(" background-color","yellow");
+	       
+	  }); 
+		});
+		*/
